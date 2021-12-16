a76b8defef3ee7aaf3167d09613ec341da60d8cb
articuno870 <abhishek.prasad871@gmail.com>
2021-12-17 00:20:09 +0530

    ic

---
+++ src/main/java/com/tutorial/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package com.tutorial.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.tutorial.spring")
+public class AppConfig {
+   
+}
